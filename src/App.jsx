7bf742333d2b66de212d2328994bbef20d91a116
andresnavia <1994andresnav@gmail.com>
2024-10-07 21:10:17 -0500

    Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,111 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import TodoComputed from "./components/TodoComputed";
-import TodoCreate from "./components/TodoCreate";
-import TodoFilter from "./components/TodoFilter";
-import TodoList from "./components/TodoList";
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
+import TodoComputed from './components/TodoComputed';
+import TodoCreate from './components/TodoCreate';
+import TodoFilter from './components/TodoFilter';
+import TodoList from './components/TodoList';
 
-const initialStateTodos = [
-    {
-        id: 1,
-        title: "Ir al Gym",
-        completed: true,
-    },
-    {
-        id: 2,
-        title: "Terminar curso de react",
-        completed: false,
-    },
-    {
-        id: 3,
-        title: "Empezar documentacion de nueva arquitectura",
-        completed: false,
-    },
-    {
-        id: 4,
-        title: "pintar cuarto",
-        completed: false,
-    },
-    {
-        id: 5,
-        title: "llevar a mantenimiento el carro",
-        completed: false,
-    },
-];
-
+// const initialStateTodos = [
+//     {
+//         id: 1,
+//         title: "Ir al Gym",
+//         completed: true,
+//     },
+//     {
+//         id: 2,
+//         title: "Terminar curso de react",
+//         completed: false,
+//     },
+//     {
+//         id: 3,
+//         title: "Empezar documentacion de nueva arquitectura",
+//         completed: false,
+//     },
+//     {
+//         id: 4,
+//         title: "pintar cuarto",
+//         completed: false,
+//     },
+//     {
+//         id: 5,
+//         title: "llevar a mantenimiento el carro",
+//         completed: false,
+//     },
+// ];
+const initialStateTodos = JSON.parse(localStorage.getItem('todos')) || [];
 const App = () => {
-    const [todos, setTodos] = useState(initialStateTodos);
-    const createTodo = (title) => {
-        const newTodo = {
-            id: Date.now(),
-            title: title,
-            completed: false,
-        };
-        setTodos([...todos, newTodo]);
+  const [todos, setTodos] = useState(initialStateTodos);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+  const createTodo = (title) => {
+    const newTodo = {
+      id: Date.now(),
+      title: title,
+      completed: false,
     };
-    const removeTodo = (id) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
-    };
-    const updateTodo = (id) => {
-        setTodos(
-            todos.map((todo) =>
-                todo.id === id
-                    ? {
-                          ...todo,
-                          completed: !todo.completed,
-                      }
-                    : todo
-            )
-        );
-    };
-    const clearCompletedTodo = () => {
-        setTodos(todos.filter((todo) => !todo.completed));
-    };
-    const [filter, setFilter] = useState("all");
-    const changeFilter = (filter) => {
-        setFilter(filter);
-    };
-    const filteredTodos = () => {
-        switch (filter) {
-            case "all":
-                return todos;
-            case "active":
-                return todos.filter((todo) => !todo.completed);
-            case "completed":
-                return todos.filter((todo) => todo.completed);
-            default:
-                return todos;
-        }
-    };
-
-    const countCompletedTodo = filteredTodos().length;
-    return (
-        <>
-            <div className="min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')]">
-                <Header />
-                <main className="container mx-auto mt-8 px-4">
-                    <TodoCreate createTodo={createTodo} />
-                    <TodoList
-                        todos={filteredTodos()}
-                        removeTodo={removeTodo}
-                        updateTodo={updateTodo}
-                    ></TodoList>
-                    <TodoComputed
-                        countCompletedTodo={countCompletedTodo}
-                        clearCompletedTodo={clearCompletedTodo}
-                    ></TodoComputed>
-                    <TodoFilter
-                        changeFilter={changeFilter}
-                        filter={filter}
-                    ></TodoFilter>
-                </main>
-
-                <footer className="mt-8 text-center dark:text-gray-400">
-                    Drag and Drop to reorder list
-                </footer>
-            </div>
-        </>
+    setTodos([...todos, newTodo]);
+  };
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const updateTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              completed: !todo.completed,
+            }
+          : todo
+      )
     );
+  };
+  const clearCompletedTodo = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
+  };
+  const [filter, setFilter] = useState('all');
+  const changeFilter = (filter) => {
+    setFilter(filter);
+  };
+  const filteredTodos = () => {
+    switch (filter) {
+      case 'all':
+        return todos;
+      case 'active':
+        return todos.filter((todo) => !todo.completed);
+      case 'completed':
+        return todos.filter((todo) => todo.completed);
+      default:
+        return todos;
+    }
+  };
+
+  const countCompletedTodo = filteredTodos().length;
+  return (
+    <>
+      <div className="-contain min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')]">
+        <Header />
+        <main className="container mx-auto mt-8 px-4 md:max-w-xl">
+          <TodoCreate createTodo={createTodo} />
+          <TodoList
+            todos={filteredTodos()}
+            removeTodo={removeTodo}
+            updateTodo={updateTodo}
+          ></TodoList>
+          <TodoComputed
+            countCompletedTodo={countCompletedTodo}
+            clearCompletedTodo={clearCompletedTodo}
+          ></TodoComputed>
+          <TodoFilter changeFilter={changeFilter} filter={filter}></TodoFilter>
+        </main>
+
+        <footer className="mt-8 text-center dark:text-gray-400">
+          Drag and Drop to reorder list
+        </footer>
+      </div>
+    </>
+  );
 };
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,32 @@
-import { useEffect, useState } from "react";
-import IconMoon from "./Icons/IconMoon";
-import IconSun from "./Icons/IconSun";
+import { useEffect, useState } from 'react';
+import IconMoon from './Icons/IconMoon';
+import IconSun from './Icons/IconSun';
 
-const initialDarkToggle = document.documentElement.className.includes("dark");
+const initialDarkToggle = document.documentElement.className.includes('dark');
 
 const Header = () => {
-    const [darkToggle, setDarkToggle] = useState(initialDarkToggle);
+  const [darkToggle, setDarkToggle] = useState(initialDarkToggle);
 
-    useEffect(() => {
-        if (darkToggle) {
-            document.documentElement.classList.add("dark");
-            localStorage.theme = "dark";
-        } else {
-            document.documentElement.classList.remove("dark");
-            localStorage.theme = "light";
-        }
-    }, [darkToggle]);
+  useEffect(() => {
+    if (darkToggle) {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = 'light';
+    }
+  }, [darkToggle]);
 
-    return (
-        <header className="container mx-auto px-4 pt-8">
-            <div className="flex justify-between">
-                <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">
-                    Todo
-                </h1>
-                <button onClick={() => setDarkToggle(!darkToggle)}>
-                    {darkToggle ? <IconSun /> : <IconMoon />}
-                </button>
-            </div>
-        </header>
-    );
+  return (
+    <header className="container mx-auto px-4 pt-8 md:max-w-xl">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">Todo</h1>
+        <button onClick={() => setDarkToggle(!darkToggle)}>
+          {darkToggle ? <IconSun /> : <IconMoon />}
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
